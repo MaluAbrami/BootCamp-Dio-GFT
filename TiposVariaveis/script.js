16452035ef4509e8381abff4cbd83481e4734ae3
MaluAbrami <marialuizaabrami@gmail.com>
2024-10-11 17:57:08 -0300

    add console.log

--- TiposVariaveis/script.js
+++ TiposVariaveis/script.js
@@ -3,4 +3,5 @@ console.log("Digite o nome do seu jogador: ");
 // Para declarar uma variavel em JS utilizamos o 'let'
 let nickname = "Maluu";
 
-console.log("Bem vindo" + nickname);
+console.log("Bem vindo " + nickname);
+console.log(nickname + " entrou no servidor");
