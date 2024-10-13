232846a226f2b2446b19f8aa6083f8b74c7abb79
MaluAbrami <marialuizaabrami@gmail.com>
2024-10-13 18:08:09 -0300

    add aula sobre operadores de atribuição

---
+++ TiposDeOperadores/atribuicao.js
@@ -0,0 +1,12 @@
+let preco = 10;
+preco += 4; //Mesma coisa que 'preco = preco + 4'
+console.log(preco);
+
+preco -= 4; //Mesma coisa que 'preco = preco - 4'
+console.log(preco);
+
+preco /= 5; //Mesma coisa que 'preco = preco / 3'
+console.log(preco);
+
+preco *= 3; //Mesma coisa que 'preco = preco * 3'
+console.log(preco);
