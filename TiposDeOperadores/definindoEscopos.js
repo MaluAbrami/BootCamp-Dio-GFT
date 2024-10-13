7df2fbe7937d78807132676f006a10d3e35ace16
MaluAbrami <marialuizaabrami@gmail.com>
2024-10-13 18:20:51 -0300

    add aula sobre definição de escopo de uma operação aritmética

---
+++ TiposDeOperadores/definindoEscopos.js
@@ -0,0 +1,10 @@
+//Diferenças de escopos e como eles afetam o resultado
+
+let resultado = 2 * 5 + 5 - 10;
+console.log(resultado);
+
+resultado = 2 * (5 + 5) - 10;
+console.log(resultado);
+
+resultado = 2 * (5 + 5 - 10);
+console.log(resultado);
