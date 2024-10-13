9b69ed33655785a74a57ae2378cc58b228ba4dd6
MaluAbrami <marialuizaabrami@gmail.com>
2024-10-13 12:36:52 -0300

    finalizada aula sobre tipos de variáveis

--- TiposDeVariáveis/script.js
+++ TiposDeVariáveis/script.js
@@ -1,3 +1,4 @@
+/* PARTE 1 - INTRODUÇÃO PARA TIPOS DE VARIÁVEIS
 let nomePokemon;
 let nivelPokemon;
 let pontosDeVidaPokemon;
@@ -14,3 +15,14 @@ pontosDeVidaPokemon = 45;
 
 //Variável Tipo Boolean, armazena 'true' ou 'false'
 selecionavel = true; 
+*/
+
+let nomePessoa = "Josicreide de Pythinho";
+let idade = 115;
+//Por boas práticas, armazenamos números como telefone, cpf e etc 
+//como tipo string, pois não faremos nenhuma operação matemática com eles
+//Além disso, sendo do tipo string podemos utilizar caracteres especiais
+let numeroTelefone = "(11)984713388"; 
+let cpf = "312.122.312-45";
+let endereco = "Rua dos Limoeiros, número 75, bairro Jaguatirica";
+let recebePensao = true;
