3f0fda03c17b459592fe58824ea70431c7892f93
MaluAbrami <marialuizaabrami@gmail.com>
2024-10-13 18:04:01 -0300

    add aula sobre operador de incremento e decremento

---
+++ TiposDeOperadores/incrementoDecremento.js
@@ -0,0 +1,11 @@
+let contador = 1;
+// '++' incremento
+contador++; //Mesma coisa que 'contador = contador + 1'
+contador++;
+console.log(contador);
+
+// '--' decremento
+contador--;
+contador--;
+contador--;
+console.log(contador);
