9288eed57ddc5607fafd751fbe6952fc5e2d25c2
MaluAbrami <marialuizaabrami@gmail.com>
2024-10-13 17:47:50 -0300

    add aula sobre operadores aritméticos

---
+++ TiposDeOperadores/aritmeticos.js
@@ -0,0 +1,30 @@
+let idade = 30 + 6;
+console.log("operação de adição " + idade);
+
+idade = 30 - 6;
+console.log("operação de subtração " + idade);
+
+let primeiroNumero = 1023;
+let segundoNumero = 23;
+console.log(primeiroNumero - segundoNumero);
+
+let precoProduto = 100.99;
+let valorComTaxa = precoProduto * 2;
+console.log(valorComTaxa);
+
+// ótimo exemplo para mostrar como funciona uma calculadora
+let multiplicador = 4;
+let multiplicando = 12;
+let produto = multiplicador * multiplicando;
+console.log("resultado da multiplicação é " + produto);
+
+multiplicador = 8;
+produto = multiplicador * multiplicando;
+console.log("resultado da multiplicação é " + produto);
+
+let notaDoMercado = 50;
+let pessoasParaDividir = 3;
+console.log("operação de divisão: " + notaDoMercado / pessoasParaDividir);
+
+let calculo = 10 % 3; //Pega o resto da divisão de 10 por 3
+console.log("Operação de módulo " + calculo);
